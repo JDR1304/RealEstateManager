6c0d0cdacebc2220cb3de7253ce45034d00ce36e
JDR <jerome.diaz-rey@coppernic.fr>
2022-08-31 18:42:04 +0200

    unit test

--- app/src/main/java/com/example/realestatemanager/Utils.java
+++ app/src/main/java/com/example/realestatemanager/Utils.java
@@ -7,10 +7,14 @@ import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.net.NetworkRequest;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.util.Log;
+
+import androidx.annotation.RequiresApi;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -63,12 +67,13 @@ public class Utils {
     }
     */
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public static boolean isInternetAvailable(Context context) {
 
-        ConnectivityManager connMgr =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         boolean isWifiConn = false;
         boolean isMobileConn = false;
+
         for (Network network : connMgr.getAllNetworks()) {
             NetworkInfo networkInfo = connMgr.getNetworkInfo(network);
             if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
@@ -79,9 +84,10 @@ public class Utils {
             }
             if (isWifiConn || isMobileConn) return true;
         }
-        Log.e("wifi status", "Wifi connected: " + isWifiConn);
-        Log.e("cellular status", "Mobile connected: " + isMobileConn);
-
         return false;
+    }
+
+    public static boolean test(boolean bool){
+        return bool;
     }
 }
--- app/src/test/java/com/example/realestatemanager/ExampleUnitTest.java
+++ app/src/test/java/com/example/realestatemanager/ExampleUnitTest.java
@@ -1,12 +1,22 @@
 package com.example.realestatemanager;
 
-import static android.content.ContentValues.TAG;
-
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import android.util.Log;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,7 +26,21 @@ import java.util.Calendar;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+
+    @Mock
+    Context mockContext;
+
+    @Mock
+    NetworkInfo mockNetworkInfo;
+
+    @Mock
+    ConnectivityManager mockConnectivityManager;
+
+    @Mock
+    Network mockNetwork;
+
     @Test
     public void getTodayDateTest() {
         String currentDate = new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
@@ -33,5 +57,32 @@ public class ExampleUnitTest {
     public void conversionEuroToDollarTest() {
         int priceInEuro = 200_000;
         assertEquals(246_305, Utils.convertEuroToDollar(priceInEuro));
+    }
+
+    @Test
+    public void isInternetAvailableTest(){
+        mockContext = mock(Context.class);
+        mockConnectivityManager = mock(ConnectivityManager.class);
+        mockNetworkInfo = mock(NetworkInfo.class);
+        mockNetwork = mock(Network.class);
+        Network [] allNetworks = mockConnectivityManager.getAllNetworks();
+
+        when(mockContext.getSystemService(anyString())).thenReturn(mockConnectivityManager);
+        when(mockConnectivityManager.getAllNetworks()).thenReturn(allNetworks);
+        when(mockConnectivityManager.getNetworkInfo(mockNetwork)).thenReturn(mockNetworkInfo);
+        when(mockNetworkInfo.getType()).thenReturn(ConnectivityManager.TYPE_WIFI);
+        when(mockNetworkInfo.isConnected()).thenReturn(true);
+
+        boolean value = Utils.isInternetAvailable(mockContext);
+
+        assertEquals(value, true);
+
+    }
+
+    @Test
+    public void test(){
+        mockContext = mock(Context.class);
+
+        //Utils.isInternetAvailable(context);
     }
 }
